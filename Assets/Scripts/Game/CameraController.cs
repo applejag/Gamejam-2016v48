f238038607df5c4799d0cf288e6e240de01d140e
kalle (jag) <kalle.jillheden@me.com>
2016-12-03 21:06:32 +0100

    Camera update
    Camera works nicely

---
+++ Assets/Scripts/Game/CameraController.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+using ExtensionMethods;
+
+public class CameraController : MonoBehaviour {
+
+	[Range(-10,90)]
+	public float angle = 45;
+	public float angleOffset;
+	public float turnLerpSpeed = 5;
+	public float tiltLerpSpeed = 2;
+	public float autoTurnMoveSpeed = 20;
+	[System.NonSerialized]
+	[Header("Raycasting")]
+	public float targetDist = 10;
+	[Range(0,1)]
+	public float hitExtraMult = .5f;
+	public LayerMask raycastLayer;
+
+	
+	public float planeAngle { get; private set; }
+	public float tiltAngle { get; private set; }
+	public float dist { get; private set; }
+
+	public Vector3 forward { get { return (planeAngle).FromDegrees().yzx(0); } }
+	public Vector3 back { get { return (planeAngle+180).FromDegrees().yzx(0); } }
+	public Vector3 left { get { return (planeAngle+270).FromDegrees().yzx(0); } }
+	public Vector3 right { get { return (planeAngle+90).FromDegrees().yzx(0); } }
+
+	void Start() {
+		if (Camera.main == null) {
+			Debug.LogError ("No camera found! Please asign a camera with the \"MainCamera\" tag!");
+			return;
+		}
+
+		planeAngle = transform.eulerAngles.y;
+		tiltAngle = angle;
+	}
+
+	void LateUpdate() {
+		if (Camera.main == null) {
+			enabled = false;
+			return;
+		}
+
+		// First calculate angles
+		angleOffset = Mathf.MoveTowardsAngle(angleOffset, 0, autoTurnMoveSpeed * Time.deltaTime);
+		float targetAngle = transform.eulerAngles.y + angleOffset;
+		planeAngle = Mathf.LerpAngle (planeAngle, targetAngle, turnLerpSpeed * Time.deltaTime);
+		tiltAngle = Mathf.LerpAngle(tiltAngle, angle, tiltLerpSpeed * Time.deltaTime);
+		Camera.main.transform.eulerAngles = new Vector3(tiltAngle, planeAngle, 0);
+
+		// Then set the distance
+		dist = Mathf.Lerp(dist, targetDist, Time.deltaTime);
+		var extra = hitExtraMult * Mathf.Clamp01(Mathf.InverseLerp(80, -10, tiltAngle));
+
+		// Lastly raycastin'
+		RaycastHit hit;
+		Ray ray = new Ray(transform.position, -Camera.main.transform.forward);
+		if (Physics.Raycast(ray, out hit, targetDist * (1+extra), raycastLayer)) {
+			Camera.main.transform.position = hit.point - ray.direction * extra * targetDist;
+		} else {
+			Camera.main.transform.position = transform.position - Camera.main.transform.forward * targetDist;
+		}
+
+	}
+
+}
---
+++ Assets/Scripts/Game/PlayerController.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+using ExtensionMethods;
+
+[RequireComponent(typeof(CharacterMovement))]
+[RequireComponent(typeof(CameraController))]
+public class PlayerController : MonoBehaviour {
+
+	public float mouseXSpeed = 1;
+	public float mouseYSpeed = 1;
+	public float mouseIdleAfter = 2;
+
+	private CharacterMovement character;
+	private CameraController _cam;
+	private float _turnAutoMoveSpeed;
+	private float _mouseIdleTime;
+
+	void Awake() {
+		character = GetComponent<CharacterMovement> ();
+		_cam = GetComponent<CameraController>();
+		_turnAutoMoveSpeed = _cam.autoTurnMoveSpeed;
+	}
+
+	void Update() {
+		Vector3 axis = _cam.forward * Input.GetAxis("Vertical")
+				+ _cam.right * Input.GetAxis("Horizontal");
+
+		// Move player
+		if (axis.magnitude > float.Epsilon)
+			character.Move (axis.normalized);
+
+		// Add mouse input to angles
+		Vector2 mouse = new Vector2(Input.GetAxis("Mouse horizontal"), Input.GetAxis("Mouse vertical"));
+		_cam.angle = Mathf.Clamp(_cam.angle + mouse.y * Time.deltaTime * mouseYSpeed, -10, 90);
+		_cam.angleOffset += mouse.x * Time.deltaTime * mouseXSpeed + character.angleDelta;
+		_cam.angleOffset %= 360;
+		character.angleDelta = 0;
+		_cam.targetDist = Mathf.Clamp(_cam.targetDist - Input.mouseScrollDelta.y, 5, 25);
+
+		// Count how long the mouse has been idle
+		if (mouse.magnitude <= float.Epsilon)
+			_mouseIdleTime += Time.deltaTime;
+		else _mouseIdleTime = 0;
+
+		// Set the auto turn speed depending on idle time
+		_cam.autoTurnMoveSpeed = _turnAutoMoveSpeed * Mathf.Clamp01(_mouseIdleTime - mouseIdleAfter);
+	}
+
+	void FixedUpdate() {
+		// Because rigidbody restrictions dont work 100%
+		transform.eulerAngles = Vector3.up * transform.eulerAngles.y;
+	}
+
+}
