30ece71a8c3c4b146884984bd98a43ba46785dbd
kalle (jag) <kalle.jillheden@me.com>
2016-12-03 17:03:55 +0100

    Character movement

---
+++ Assets/Scripts/Game/PlayerInput.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(CharacterMovement))]
+public class PlayerInput : MonoBehaviour {
+
+	private CharacterMovement character;
+
+	void Awake() {
+		character = GetComponent<CharacterMovement> ();
+	}
+
+	void Update() {
+		Vector3 axis = Vector3.zero;
+		if (Input.GetKey (KeyCode.W)) {
+			axis.z += 1;
+		}
+		if (Input.GetKey (KeyCode.S)) {
+			axis.z -= 1;
+		}
+		if (Input.GetKey (KeyCode.D)) {
+			axis.x += 1;
+		}
+		if (Input.GetKey (KeyCode.A)) {
+			axis.x -= 1;
+		}
+
+		if (axis.magnitude > float.Epsilon)
+			character.Move (axis.normalized);
+	}
+
+}
