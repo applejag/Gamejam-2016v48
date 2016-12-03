3bc7265172684f1d90804e1613c85c8c4bc5d89e
kalle (jag) <kalle.jillheden@me.com>
2016-12-03 15:07:15 +0100

    Added utility

---
+++ Assets/Scripts/AppInfo/Common.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace AppInfo {
+	public static class Common {
+		public const string NAME = "Södertörn gamejam spel";
+		public const string AUTHOR = null;
+		public const string WEBSITE = null;
+		public static readonly Version VERSION = new Version(Version.Label.Prototype, 0, 0, 0);
+	}
+}
---
+++ Assets/Scripts/Game/GamePresets.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[DisallowMultipleComponent]
+public sealed class GamePresets : SingletonBase<GamePresets> {
+
+	[Header("Existing objects")]
+	public Canvas _canvas;
+	private RectTransform _canvasRect;
+
+	// Existing objects
+	public static Canvas canvas { get { return instance._canvas; } }
+	public static RectTransform canvasRect { get { return instance._canvasRect; } }
+
+	protected override void Awake() {
+		base.Awake();
+		_canvasRect = _canvas.GetComponent<RectTransform>();
+	}
+}
