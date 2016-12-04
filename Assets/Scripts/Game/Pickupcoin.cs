ded40aea719dbac786ae3076e920d062e4f0aea0
Hortencya <ingeborg.asplund@gmail.com>
2016-12-04 18:01:35 +0100

    Plattform are reduced
    Fewer but working plattforms and a coin is added the coin do not work
    yet

---
+++ Assets/Scripts/Game/Pickupcoin.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pickupcoin : MonoBehaviour {
+
+	// Use this for initialization
+	void OnTriggerEnter()
+    {
+        // Currency object shall disappear
+        // Update to global currency script as increase of currency
+        // eventually display the change in the UI
+        GameObject.Destroy(this);
+    }
+   
+}
